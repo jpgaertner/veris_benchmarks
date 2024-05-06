720a65cec4caf3f833cecd41ffb1ecdceac031e6
Jan Gärtner <95316997+jpgaertner@users.noreply.github.com>
2024-05-06 17:23:45 +0200

    Update job_mitgcm_levante.sh

--- job_mitgcm_levante.sh
+++ job_mitgcm_levante.sh
@@ -2,17 +2,20 @@
 #SBATCH --account=bk1377
 #SBATCH --job-name=mitgcm_seaice
 #SBATCH --partition=compute
-#SBATCH -t 1:00:00
+#SBATCH -t 0:10:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=2
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH -o output.txt
 #SBATCH -e output.txt
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jan.gaertner@awi.de
+#SBATCH --mem=5G
 
 
-ulimit -s unlimited
+ulimit -c 0
+ulimit -s 1067008 # 1GB
+ulimit -l 1067008 # 1GB
 
 export OMP_NUM_THREADS=1
 cd ${SLURM_SUBMIT_DIR}
